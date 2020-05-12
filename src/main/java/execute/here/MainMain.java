c0d323904baf3fe14999689e972bc0ade3c57f70
Vetal4ik <havrilyukvv@gmail.com>
2020-05-12 22:01:51 +0300

    Add some things

--- src/main/java/execute/here/MainMain.java
+++ src/main/java/execute/here/MainMain.java
@@ -14,15 +14,21 @@ public class MainMain {
         System.out.println();
         System.out.println("Task №2:");
         TaskNumberTwo.main(args);
+        System.out.println();
         System.out.println("Task №3:");
         TaskNumberThree.main(args);
+        System.out.println();
         System.out.println("Task №4:");
         TaskNumberFour.main(args);
+        System.out.println();
         System.out.println("Task №5:");
         TaskNumberFive.main(args);
+        System.out.println();
         System.out.println("Task №6:");
         TaskNumberSix.main(args);
+        System.out.println();
         System.out.println("Task №7:");
         TaskNumberSeven.main(args);
+        System.out.println();
     }
 }
