b1d7a123eb97fcc5ef52d2fe2646fc632d4abaaf
Vetal4ik <havrilyukvv@gmail.com>
2020-05-13 15:28:28 +0300

    some changes

--- src/main/java/seventh/task/TaskNumberSeven.java
+++ src/main/java/seventh/task/TaskNumberSeven.java
@@ -13,7 +13,6 @@ public class TaskNumberSeven {
         for (int i = length - 1; i >= 0; i-- ){
             reverse = reverse + original.charAt(i);
         }
-
         if (original.equals(reverse)) {
             System.out.println("YES");
         }
