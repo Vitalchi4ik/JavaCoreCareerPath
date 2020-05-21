076e7637b2ed7c0bd1ee6999f5779cdd17d6c539
Vitalchi4ik <havrilyukvv@gmail.com>
2020-05-21 16:29:30 +0300

    Refactor thirteen task

--- src/main/java/thirteenth/TaskNumberThirteen.java
+++ src/main/java/thirteenth/TaskNumberThirteen.java
@@ -6,40 +6,87 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 
 public class TaskNumberThirteen {
-    public static void main(String[] args) {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.print("Enter command with operands, like X + Y: ");
-            String str = br.readLine();
 
-            ArrayList<String> stringList = new ArrayList<String>();
-            for(String parserStrings : str.split(" "))
-            {
-                stringList.add(parserStrings);
+    public static int add(int a, int b){
+        int sumNum = a + b;
+        return sumNum;
+    }
+
+    public static int subtract(int a, int b){
+        int subtractNum = a - b;
+        return subtractNum;
+    }
+
+    public static int multiplication(int a, int b){
+        int multiplicationNum = a * b;
+        return multiplicationNum;
+    }
+
+    public static int division(int a, int b){
+        int divisionNum = a / b;
+        return divisionNum;
+    }
+
+    public static int pow(int a, int b){
+        int powNum = a ^ b;
+        return powNum;
+    }
+
+    public static int log (int a){
+        int logNum = (int) Math.log(a);
+        return logNum;
+    }
+
+    public static int sqrt (int a){
+        int sqrtNum = (int) Math.sqrt(a);
+        return sqrtNum;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter command with operands, like X + Y or for \"log\" or \"sqt\" like X sqrt: ");
+        String str = br.readLine();
+
+        ArrayList<String> stringList = new ArrayList<String>();
+        for(String parserStrings : str.split(" "))
+        {
+            stringList.add(parserStrings);
+        }
+
+        if(stringList.size()>3){
+            System.out.println("Too much symbols");
+        }
+        else if (stringList.size()==2){
+            int z = Integer.parseInt(stringList.get(0));
+            String mathOperator = stringList.get(1);
+            int mathRezultForLogSqrt = 0;
+            if (mathOperator.equals("log")){
+                mathRezultForLogSqrt = log(z);
             }
-            if(stringList.size()>3){
-                System.out.println("Too much symbols");
+            else if(mathOperator.equals("sqrt")){
+                mathRezultForLogSqrt = sqrt(z);
             }
-            else{
-                int x = Integer.parseInt(stringList.get(0));
-                int y = Integer.parseInt(stringList.get(2));
-                String operator = stringList.get(1);
-                int result;
-                if (operator.equals("+")) {
-                    result = x + y;
+            System.out.println(mathRezultForLogSqrt);
+        }
+        else{
+            int x = Integer.parseInt(stringList.get(0));
+            int y = Integer.parseInt(stringList.get(2));
+            String operator = stringList.get(1);
+            int result;
+            if (operator.equals("+")) {
+                result = add(x,y);
             } else if (operator.equals("-")){
-                    result = x - y;
+                result = subtract(x, y);
             } else if (operator.equals("*")){
-                    result = x * y;
+                result = multiplication(x, y);
             } else if (operator.equals("/")){
-                    result = x/y;
+                result = division(x, y);
+            } else if (operator.equals("pow")){
+                result = pow(x, y);
             } else{
                 throw new java.lang.Error("Operator not recognized");
             }
-                System.out.println(result);
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(result);
         }
     }
 }
